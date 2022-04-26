69cfec2a3c9f0c79363e2df874f0f2be1ead264f
Nahuel Rojas <nahuel.diego.rojas@gmail.com>
2022-04-26 19:11:37 -0300

    Actualizacion 3

--- Prueba/src/Prueba.h
+++ Prueba/src/Prueba.h
@@ -13,11 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-<<<<<<< HEAD
-void getFloat(char mensaje[], char mensajeError[], int minimo, int maximo, int *numeroIngresado);
-=======
 float getFloat(char mensaje[], char mensajeError[], float minimo, float maximo, float *numeroIngresado);
->>>>>>> 53cc4cb (actualizacion 2)
 int myGets(char* cadena, int longitud);
 
 
